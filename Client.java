9cb52b1032f587b9a3942f3b5f43c5af6d36e7b8
JohKaro <163749123+JohKaro@users.noreply.github.com>
2024-12-12 16:47:02 +0200

    change 8 extensions

--- Client.java
+++ Client.java
@@ -1,8 +1,14 @@
 public class Client {//
 
     private Person person;
+
     public Client(Person person) {
         this.person = person;
     }
+
+    public String getName() {
+        return person.getName();
+    }
+}
 
 }
--- ClientFactory.java
+++ ClientFactory.java
@@ -1,11 +1,12 @@
 public class ClientFactory {
-    public class ClientFactory {
 
-        public static Client createClient(Person person) throws InvalidAgeException {
-            if (person.getAge() < 18) {
-                throw new InvalidAgeException("Client must be at least 16 years old!");
-            }
-            return new Client(person);
+    public static Client createClient(Person person) throws InvalidAgeException {
+        // בדיקת גיל
+        if (person.getAge() < 18) {
+            throw new InvalidAgeException("Client must be at least 18 years old!");
         }
+
+        // יצירת לקוח חדש
+        return new Client(person);
     }
 }
