2b8b703ffba088c65bddd6668a36a543c4a629af
daelHacohen <dael12345@gmail.com>
2024-12-23 22:40:52 +0200

    update 2.6

--- Gym.java
+++ Gym.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Gym {//
     private static Gym instance;
     private Secretary gymSecretary;
@@ -37,21 +39,28 @@ public Secretary getSecretary(){
 
     @Override
     public String toString() {
-        String outPut ="Gym Name: "+this.gymName+"\nGym Secretary: ID: 1113 | Name: "+gymSecretary.getPerson().getName()+" | Gender: "+gymSecretary.getPerson().getGender()+" | Birthday: "+ getSecretary().getPerson().getDate_of_birth()+" | Age: "+gymSecretary.getPerson().getAge()+" | Balance: "+ gymSecretary.getPerson().getBalance()+" | Role: Secretary | Salary per Month: "+gymSecretary.getSalary()+"\nGym Balance: "+gymSecretary.getGymBalanc()+"\n\n";
+        String outPut ="Gym Name: "+this.gymName+"\nGym Secretary: ID: "+gymSecretary.getPerson().getId()+" | Name: "+gymSecretary.getPerson().getName()+" | Gender: "+gymSecretary.getPerson().getGender()+" | Birthday: "+ getSecretary().getPerson().getDate_of_birth()+" | Age: "+gymSecretary.getPerson().getAge()+" | Balance: "+ gymSecretary.getPerson().getBalance()+" | Role: Secretary | Salary per Month: "+gymSecretary.getSalary()+"\nGym Balance: "+gymSecretary.getGymBalanc()+"\n\n";
         outPut+="Clients Data:\n";
         for (Client client: gymSecretary.getGymClientList()) {
-            outPut+="ID: 1112 | Name: "+client.getName()+" | Gender: "+client.getPerson().getGender()+" | Birthday: "+client.getPerson().getDate_of_birth()+" | Age: "+client.getPerson().getAge()+" | Balance: "+client.getPerson().getBalance()+"\n";
+            outPut+="ID: "+client.getPerson().getId()+" | Name: "+client.getName()+" | Gender: "+client.getPerson().getGender()+" | Birthday: "+client.getPerson().getDate_of_birth()+" | Age: "+client.getPerson().getAge()+" | Balance: "+client.getPerson().getBalance()+"\n";
         }
         outPut+="\nEmployees Data:\n";
         for (Instructor instructor: gymSecretary.getGymInstructorList()){
-            outPut+="ID: 1112 | Name: "+instructor.getInstructorPerson().getName()+" | Gender: "+instructor.getInstructorPerson().getGender()+" | Birthday: "+instructor.getInstructorPerson().getDate_of_birth()+" | Age: "+instructor.getInstructorPerson().getAge()+" | Balance: "+instructor.getInstructorPerson().getBalance()+" | Role: Instructor | Salary per Hour: "+instructor.getSalaryPerHour()+" | Certified Classes: "+instructor.getSessionQualified().toString()+"\n";
+            outPut+="ID: "+instructor.getInstructorPerson().getId()+" | Name: "+instructor.getInstructorPerson().getName()+" | Gender: "+instructor.getInstructorPerson().getGender()+" | Birthday: "+instructor.getInstructorPerson().getDate_of_birth()+" | Age: "+instructor.getInstructorPerson().getAge()+" | Balance: "+instructor.getInstructorPerson().getBalance()+" | Role: Instructor | Salary per Hour: "+instructor.getSalaryPerHour()+" | Certified Classes:"+arrayToString(instructor.getSessionQualified())+"\n";
         }
-       outPut+="ID: 1113 | Name: "+gymSecretary.getPerson().getName()+" | Gender: "+gymSecretary.getPerson().getGender()+" | Birthday: "+ getSecretary().getPerson().getDate_of_birth()+" | Age: "+gymSecretary.getPerson().getAge()+" | Balance: "+ gymSecretary.getPerson().getBalance()+" | Role: Secretary | Salary per Month: "+gymSecretary.getSalary()+"\n\n";
+       outPut+="ID: "+gymSecretary.getPerson().getId()+" | Name: "+gymSecretary.getPerson().getName()+" | Gender: "+gymSecretary.getPerson().getGender()+" | Birthday: "+ getSecretary().getPerson().getDate_of_birth()+" | Age: "+gymSecretary.getPerson().getAge()+" | Balance: "+ gymSecretary.getPerson().getBalance()+" | Role: Secretary | Salary per Month: "+gymSecretary.getSalary()+"\n\n";
         outPut+="Sessions Data:\n";
         for (Session session: gymSecretary.getSessionList()){
             outPut+="Session Type: "+session.getSessionType()+" | Date: "+session.dateAndHour+" | Forum: "+session.getForumType()+" | Instructor: "+session.thisSessionInstructor.getInstructorPerson().getName()+" | Participants: "+session.getListOfClientsInCurrentClass().size()+"/"+session.getNumber_of_people_in_the_class()+"\n";
         }
 
         return outPut;
+    }
+    public String arrayToString(ArrayList<SessionType>sessionQualifiedList){
+        String str = " "+sessionQualifiedList.get(0);
+        for (int i = 1; i < sessionQualifiedList.size(); i++) {
+            str+= ", "+sessionQualifiedList.get(i);
+        }
+        return str;
     }
 }
